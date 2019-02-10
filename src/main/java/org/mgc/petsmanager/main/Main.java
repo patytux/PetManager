21b1e2f46a2dba8e37a8685759019ab7ecb26fa7
patytux <patytux@gmail.com>
2019-02-10 08:56:24 -0400

    Init project

---
+++ src/main/java/org/mgc/petsmanager/main/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mgc.petsmanager.main;
+
+/**
+ *
+ * @author patytux
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Init");
+    }
+}
